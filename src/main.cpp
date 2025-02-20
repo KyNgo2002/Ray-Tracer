9fad18baf0512b36733f7cb2af77ffbf04ab7689
Ky <kyngo992@gmail.com>
2025-02-19 23:02:47 -0800

    GLM and shaders

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <GLFW/glfw3.h>
 #include "windows.h"
 #include "../include/Shader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 const unsigned WINDOW_SIZE = 900;
 
