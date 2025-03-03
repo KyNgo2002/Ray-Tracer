87f1aa73525994775c1925d562b606eb9795b307
Ky <kyngo992@gmail.com>
2025-03-02 19:55:53 -0800

    object loader

---
+++ include/Model.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Shader.h";
+#include "Mesh.h"
+#include "stb_image.h"
+
+class Model {
+public:
+    Model(char* path);
+    void Draw(Shader* shader);
+private:
+    // model data
+    std::vector<Mesh> meshes;
+    std::string directory;
+
+    void loadModel(std::string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        std::string typeName);
+};
+
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -34,8 +34,22 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std:
 }
 
 void Mesh::Draw(Shader* shader) {
-	shader->use();
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (unsigned int i = 0; i < textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i); 
+		std::string number;
+		std::string name = textures[i].type;
+		if (name == "texture_diffuse")
+			number = std::to_string(diffuseNr++);
+		else if (name == "texture_specular")
+			number = std::to_string(specularNr++);
 
+		shader->setInt(("material." + name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
 
 	// Draw
 	glBindVertexArray(VAO);
