8cba10cfcc112afad7746ea26e46ef7ec9ba2ce0
Ky <kyngo992@gmail.com>
2025-03-06 16:28:49 -0800

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,15 @@
 
 #include <../include/Camera.h>
 #include "../include/OpenGL.h"
-#include "../include/stb_image.h"
 #include "../include/Model.h"
 #include "Cube.h"
-
-//unsigned int loadTexture(char const* path);
 
 int main() {
     OpenGL openGL("Shaders/Vert.vert", "Shaders/Frag.frag", "Shaders/LightVert.vert", "Shaders/LightFrag.frag");
     
-    Shader* cubeShader = openGL.getShader();
+    Shader modelShader("Shaders\\ModelLoading.vert", "Shaders\\ModelLoading.frag");
     Shader* lightShader = openGL.getLightShader();
+   
     Camera* camera = openGL.getCamera();
 
     Cube cube(camera, openGL.getScreenWidth(), openGL.getScreenHeight());
@@ -29,65 +27,15 @@ int main() {
 
     // MVP matrices
     glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 view = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+    modelShader.setMat4("model", model);
     glm::mat4 projection = 
         glm::perspective(glm::radians(45.0f), (float)openGL.getScreenWidth() / (float)openGL.getScreenHeight(), 0.1f, 100.0f);
 
-    /*unsigned int diffuseMap = loadTexture("./Textures/container.png");
-    unsigned int specularMap = loadTexture("./Textures/container1.png");*/
-
-    /*cubeShader->use();
-    cubeShader->setInt("material.diffuse", 0);
-    cubeShader->setInt("material.specular", 1);
-    cubeShader->setFloat("material.shininess", 64.0f);
-
-    cubeShader->setMat4("model", model);
-    cubeShader->setMat4("view", camera->lookAt);
-    cubeShader->setMat4("projection", projection);*/
-    
-    //// point light 1
-    //cubeShader->setVec3("pointLights[0].position", pointLightPositions[0]);
-    //cubeShader->setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-    //cubeShader->setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-    //cubeShader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-    //cubeShader->setFloat("pointLights[0].constant", 1.0f);
-    //cubeShader->setFloat("pointLights[0].linear", 0.09f);
-    //cubeShader->setFloat("pointLights[0].quadratic", 0.032f);
-    //// point light 2
-    //cubeShader->setVec3("pointLights[1].position", pointLightPositions[1]);
-    //cubeShader->setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-    //cubeShader->setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
-    //cubeShader->setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
-    //cubeShader->setFloat("pointLights[1].constant", 1.0f);
-    //cubeShader->setFloat("pointLights[1].linear", 0.09f);
-    //cubeShader->setFloat("pointLights[1].quadratic", 0.032f);
-    //// point light 3
-    //cubeShader->setVec3("pointLights[2].position", pointLightPositions[2]);
-    //cubeShader->setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-    //cubeShader->setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
-    //cubeShader->setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
-    //cubeShader->setFloat("pointLights[2].constant", 1.0f);
-    //cubeShader->setFloat("pointLights[2].linear", 0.09f);
-    //cubeShader->setFloat("pointLights[2].quadratic", 0.032f);
-    //// point light 4
-    //cubeShader->setVec3("pointLights[3].position", pointLightPositions[3]);
-    //cubeShader->setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-    //cubeShader->setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
-    //cubeShader->setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
-    //cubeShader->setFloat("pointLights[3].constant", 1.0f);
-    //cubeShader->setFloat("pointLights[3].linear", 0.09f);
-    //cubeShader->setFloat("pointLights[3].quadratic", 0.032f);
-
-    //cubeShader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-    //cubeShader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-    //cubeShader->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-    //cubeShader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
-
-    // Model 
-    Shader modelShader("Shaders\\ModelLoading.vert", "Shaders\\ModelLoading.frag");
-
     modelShader.setMat4("view", camera->lookAt);
     modelShader.setMat4("projection", projection); 
+
     char path[] = "C:\\Users\\kyngo\\Downloads\\backpack\\backpack.obj";
 
     // Model loading
@@ -114,33 +62,15 @@ int main() {
         processInput(openGL.getWindow(), camera, deltaTime);
 
         modelShader.use();
-        model = glm::mat4(1.0f);
+        glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
         modelShader.setMat4("model", model);
         modelShader.setMat4("view", camera->lookAt);
-        glm::mat4 projection =
-            glm::perspective(glm::radians(45.0f), (float)openGL.getScreenWidth() / (float)openGL.getScreenHeight(), 0.1f, 100.0f);
         modelShader.setMat4("projection", projection);
 
         backpack.Draw(&modelShader);
         cube.draw(lightShader);
-        // Regular Cube Shader setup
-        /*cubeShader->use();
-        cubeShader->setVec3("ViewPos", camera->camPosition);
-        cubeShader->setMat4("view", camera->lookAt);
-        
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, diffuseMap);
-
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, specularMap);*/
-
-        // Regular Cube Render
-       /* glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);*/
-
-
 
         calculateFPS(runningFrameCount, totalFrames);
 
@@ -149,48 +79,6 @@ int main() {
         glfwPollEvents();
     }
 
-    // Clean vertex arrays
-    /*glDeleteVertexArrays(1, &cubeVAO);
-    glDeleteBuffers(1, &VBO);*/
-
     glfwTerminate();
 	return 0;
 }
-
-//unsigned int loadTexture(char const* path) {
-//    unsigned int textureID;
-//    glGenTextures(1, &textureID);
-//
-//    int width, height, nrComponents;
-//    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
-//    if (data)
-//    {
-//        GLenum format;
-//        if (nrComponents == 1)
-//            format = GL_RED;
-//        else if (nrComponents == 3)
-//            format = GL_RGB;
-//        else if (nrComponents == 4)
-//            format = GL_RGBA;
-//
-//        glBindTexture(GL_TEXTURE_2D, textureID);
-//        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-//        glGenerateMipmap(GL_TEXTURE_2D);
-//
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//        std::cout << "Successfully loaded texture file" << std::endl;
-//
-//        stbi_image_free(data);
-//    }
-//    else
-//    {
-//        std::cout << "Texture failed to load at path: " << path << std::endl;
-//        stbi_image_free(data);
-//    }
-//
-//    return textureID;
-//}
