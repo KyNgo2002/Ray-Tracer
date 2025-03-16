6b34175d6f1b8d4517ba9400248bbd264cbcf95d
Ky <kyngo992@gmail.com>
2025-03-15 19:37:02 -0700

    CLEANER

--- src/Spheres.cpp
+++
@@ -1,9 +0,0 @@
-#include "Spheres.h"
-
-Spheres::Spheres(Camera* camera, float scrWidth, float scrHeight) :
-    camera{ camera },
-    scrWidth{ scrWidth },
-    scrHeight{ scrHeight },
-    projection{ glm::perspective(glm::radians(45.0f), scrWidth / scrHeight, 0.1f, 100.0f) } {
-
-}
