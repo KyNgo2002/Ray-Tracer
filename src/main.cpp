9a3087c7a50e235bac9201635861e9a1523f413d
Ky <kyngo992@gmail.com>
2025-03-25 21:34:41 -0700

    i guess

--- src/main.cpp
+++ src/main.cpp
@@ -171,10 +171,10 @@ int main() {
 
     std::vector<Material> materials = {
         {glm::vec3{1.0f, 1.0f, 1.0f}, 0.0f, glm::vec3{1.0f, 1.0f, 1.0f}, 2.0f},
-        {glm::vec3{1.0f, 1.0f, 1.0f}, 1.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 0.0f},
-        {glm::vec3{1.0f, 0.0f, 1.0f}, 1.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 0.0f},
-        {glm::vec3{0.0f, 1.0f, 0.0f}, 1.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 0.0f},
-        {glm::vec3{1.0f, 0.0f, 0.0f}, 1.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 0.0f}
+        {glm::vec3{1.0f, 1.0f, 1.0f}, 1.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 1.0f},
+        {glm::vec3{0.5f, 0.0f, 0.5f}, 0.1f, glm::vec3{0.0f, 0.0f, 0.0f}, 1.0f},
+        {glm::vec3{0.0f, 0.5f, 0.0f}, 0.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 1.0f},
+        {glm::vec3{0.5f, 0.0f, 0.0f}, 0.0f, glm::vec3{0.0f, 0.0f, 0.0f}, 1.0f}
     };
 
     std::vector<std::string> materialNames = {
